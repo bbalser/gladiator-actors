8470ee49cad265eee88dc33ea154a6ef71f49a02
Brian Balser <brian.balser@gmail.com>
2014-07-25 22:41:04 -0400

    Equippable can now have access to attacker and defender during attack sequence

--- src/main/scala/battle/Equippable.scala
+++ src/main/scala/battle/Equippable.scala
@@ -1,28 +1,40 @@
 package battle
 
-import battle.Equippable.{Attack, AdjustmentType}
+import battle.Equippable.{Defense, Attack, AdjustmentType}
+
+import scala.util.DynamicVariable
 
 trait Equippable {
 
-  class Adjustment(val adjustmentType: AdjustmentType, val value: Int)
+  var adjustments: List[Adjustment] = Nil
+  private val dynamicDefender = new DynamicVariable[Gladiator](null)
+  private val dynamicAttacker = new DynamicVariable[Gladiator](null)
+
+  def defender = dynamicDefender.value
+  def attacker = dynamicAttacker.value
+
+  class Adjustment(val adjustmentType: AdjustmentType, thunk: => Int) {
+    def value = thunk
+  }
 
   class AdjustmentApi(adjustmentType: AdjustmentType) {
-    def +(x: Int): Adjustment = new Adjustment(adjustmentType, x)
-    def -(x: Int): Adjustment = new Adjustment(adjustmentType, -x)
+    def +(x: => Int): Unit = add(new Adjustment(adjustmentType, x))
+    def -(x: => Int): Unit = add(new Adjustment(adjustmentType, -x))
   }
 
-  def attack: AdjustmentApi = {
-    new AdjustmentApi(Attack)
+  def attack: AdjustmentApi = new AdjustmentApi(Attack)
+  def defend: AdjustmentApi = new AdjustmentApi(Defense)
+
+
+  def adjustment(adjustmentType: AdjustmentType, attacker: Gladiator, defender: Gladiator): Integer = {
+    dynamicAttacker.withValue(attacker) {
+      dynamicDefender.withValue(defender) {
+        adjustments.filter(_.adjustmentType == adjustmentType).map(_.value).sum
+      }
+    }
   }
 
-  object whenAttacking {
-    def apply(newAdjustments: Adjustment*) = adjustments = adjustments ++ newAdjustments
-  }
-
-  var adjustments: List[Adjustment] = Nil
-  def adjustment(adjustmentType: AdjustmentType): Integer = {
-    adjustments.filter(_.adjustmentType == adjustmentType).map(_.value).sum
-  }
+  private def add(adjustment: Adjustment) = adjustments = adjustments :+ adjustment
 
 }
 
@@ -30,6 +42,7 @@ object Equippable {
 
   sealed trait AdjustmentType
   object Attack extends AdjustmentType
+  object Defense extends AdjustmentType
 
 }
 
--- src/test/scala/battle/EquippableSpec.scala
+++ src/test/scala/battle/EquippableSpec.scala
@@ -1,6 +1,6 @@
 package battle
 
-import battle.Equippable.Attack
+import battle.Equippable.{Defense, Attack}
 import org.scalatest.{Matchers, FlatSpecLike}
 
 class EquippableSpec extends Matchers with FlatSpecLike {
@@ -8,22 +8,52 @@ class EquippableSpec extends Matchers with FlatSpecLike {
   "An Equippable" should "add 1 to attackRoll when defined with attack + 1 " in {
 
     object TestSword extends Equippable {
-      whenAttacking {
-        attack + 1
-      }
+      attack + 1
     }
 
-    TestSword.adjustment(Attack) should be (1)
+    TestSword.adjustment(Attack, null, null) should be (1)
   }
 
   it should "be allowed to subtract one from attack as well" in {
     object TestSword extends Equippable {
-      whenAttacking {
-        attack - 1
-      }
+      attack - 1
     }
 
-    TestSword.adjustment(Attack) should be (-1)
+    TestSword.adjustment(Attack, null, null) should be (-1)
+  }
+
+  it should "get access to defender to help in decision making" in {
+    object TestSword extends Equippable {
+      attack + {defender.name match {
+          case "John" => 2
+          case _ => 1
+      }}
+    }
+
+    TestSword.adjustment(Attack, null, Gladiator("John")) should be (2)
+    TestSword.adjustment(Attack, null, Gladiator("David")) should be (1)
+  }
+
+  it should "allow additions to Defense" in {
+    object TestShield extends Equippable {
+      defend + 1
+    }
+
+    TestShield.adjustment(Defense, null, null) should be (1)
+  }
+
+  it should "have access to attacker to help make decisions" in {
+    object TestShield extends Equippable {
+      defend + {attacker.hitpoints match {
+        case x if x > 3 => 2
+        case _ => 1
+      }}
+    }
+
+    val attacker = Gladiator("John")
+    TestShield.adjustment(adjustmentType = Defense, attacker = attacker, defender = Gladiator("Mary")) should be (2)
+    attacker.applyDamage(2)
+    TestShield.adjustment(adjustmentType = Defense, attacker = attacker, defender = Gladiator("Mary")) should be (1)
   }
 
 }
